d127b6e23b318655067489e88b9a6df4d81b9c6a
codej625 <codej625@gmail.com>
2023-11-08 03:29:00 +0900

    20231108 update

---
+++ script.js
@@ -0,0 +1,13 @@
+/* iife *************************************************************************/
+(() => {
+  dday();
+})();
+/* function *********************************************************************/
+function dday() {
+  var targetDate = new Date("2023-07-20");
+  var currentDate = new Date();
+  var timeDiff = currentDate - targetDate;
+  var currentDday = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  return (document.getElementById("d-day").textContent = `진우🩷진주 ${currentDday+2}일`);
+}
+/* function *********************************************************************/
