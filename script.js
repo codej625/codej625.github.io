d01cab147f3f551a103408f7eb1b3540f5991fb5
codej625 <codej625@gmail.com>
2024-04-16 11:36:15 +0900

    20240416 update

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-/* iife *************************************************************************************/
 (() => {
   dday();
 })();
-/* function *********************************************************************************/
+
 function dday() {
   const now = new Date();
   const start = new Date("2023-07-20");
   const timeDiff = now.getTime() - start.getTime();
-  const day = Math.floor(timeDiff / (1000 * 60 * 60 * 24) + 1);
-  return (document.getElementById("d-day").textContent = `진우🩷진주 ${day}일`);
+  const day = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  return (document.getElementById("d-day").textContent = `진우🩷진주 ${day+1}일`);
 }
-/* function *********************************************************************************/
